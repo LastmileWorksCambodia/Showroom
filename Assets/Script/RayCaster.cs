a099ebadd5a67c5e9336b2d8450c2168e2c54033
Preuk <hangathikpreuk@gmail.com>
2022-06-30 13:34:33 +0700

    add interior asset, build light and create door, ray cast script

---
+++ Assets/Script/RayCaster.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCaster : MonoBehaviour
+{
+    [SerializeField] float rayDistance = 20f;
+    [SerializeField] LayerMask layerMask;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitInfo, rayDistance, layerMask))
+            {
+                if (hitInfo.collider.gameObject.tag == "Lamp")
+                {
+                    Debug.Log("hit Lamp");
+                }
+
+                if (hitInfo.collider.gameObject.tag == "Door")
+                {
+                     if (hitInfo.collider.TryGetComponent<Door>(out Door door))
+                    {
+                        if (door.IsOpen)
+                        {
+                            door.Close();
+                        }
+                        else
+                        {
+                            door.Open(transform.position);
+                        }
+                    }
+                    Debug.Log("hit Door");
+                }
+
+                else
+                {
+                    return;
+                }
+            }
+
+            else
+            {
+                Debug.Log("hit nothing");
+            }
+        }
+    }
+}
